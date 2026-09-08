146b34cf5a6c43421c3db239530048895e8df99a
Swaraj Jagtap <swarajjagtap22@gmail.com>
2026-09-08 23:28:29 +0530

    Add files via upload

---
+++ Constructors_Demo.java
@@ -0,0 +1,21 @@
+class Demo
+{
+    public Demo()
+    {
+        System.out.println("Inside Default Constructor");
+    }
+    public Demo(int i,int j)
+    {
+        System.out.println("Inside Parametrized Constructor");
+    }
+}
+
+class Constructors_Demo
+{
+    public static void main(String A[])
+    {
+        Demo dobj1 = new Demo();
+        Demo dobj2 = new Demo(11,21);
+
+    }
+}
